fc9f439eda9ff8adf1d92bb208c661dc75c29628
koroinu <nk.yuuka1109@gmail.com>
2024-03-31 01:54:12 +0900

    ind commit

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ def add_todo():
     return redirect(url_for('index'))
 
 @app.route('/edit/<int:todo_id>')
+def edit_todo(todo_id):
+    if 1 <= todo_id <= len(todos):
+        return render_template('edit.html', todo=todos[todo_id-1], todo_id=todo_id)
+    else:
+        return redirect(url_for('index'))
 
+@app.route('/update/<inr:todo_id>', methods=['POST'])
+def update_todo(todo_id):
+    if 1 <= todo_id <= len(todos):
+        todo = request.form.get('todo')
+        todos[todo_id-1] = todo
+    return redirect(url_for('index'))
 
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    if 1 <= todo_id <= len(todos):
+        del todos[todo_id-1]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
